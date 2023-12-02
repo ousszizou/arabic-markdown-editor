7553ecfada67fee0269c09d20b4ebb8e7ff05bd1
ousszizou <odjaidri@gmail.com>
2023-12-02 15:24:00 +0100

    migrate to nuxt 3 - configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,78 +1,70 @@
-export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'markdown-editor',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'codemirror/lib/codemirror.css',
-    'codemirror/theme/ayu-mirage.css',
-    'codemirror/addon/scroll/simplescrollbars.css',
-    'highlight.js/styles/nord.css'
-    // "@/assets/css/tailwind.css"
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/vuex-persist', ssr: false },
-    { src: '~plugins/nuxt-codemirror', ssr: false },
-    { src: '~plugins/clickaway.js', ssr: false },
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/tailwindcss'
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
+export default defineNuxtConfig({
   modules: [
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    '@nuxtjs/markdownit'
+    '@pinia/nuxt',
+    // '@nuxtjs/tailwindcss',
+    // '@nuxtjs/pwa',
+    // '@nuxtjs/markdownit'
   ],
+});
+// export default {
+//   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+//   ssr: false,
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en'
-    }
-  },
+//   // Target: https://go.nuxtjs.dev/config-target
+//   target: 'static',
 
-  markdownit: {
-    runtime: true,
-    preset: 'default',
-    html: true,
-    linkify: true,
-    xhtmlOut: true,
-    breaks: true,
-    typographer: true,
-    use: [
-      'markdown-it-div',
-      'markdown-it-attrs',
-      'markdown-it-emoji',
-      'markdown-it-highlightjs'
-    ]
-  },
+//   // Global page headers: https://go.nuxtjs.dev/config-head
+//   head: {
+//     title: 'markdown-editor',
+//     meta: [
+//       { charset: 'utf-8' },
+//       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//       { hid: 'description', name: 'description', content: '' }
+//     ],
+//     link: [
+//       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+//     ]
+//   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+//   // Global CSS: https://go.nuxtjs.dev/config-css
+//   css: [
+//     'codemirror/lib/codemirror.css',
+//     'codemirror/theme/ayu-mirage.css',
+//     'codemirror/addon/scroll/simplescrollbars.css',
+//     'highlight.js/styles/nord.css'
+//     // "@/assets/css/tailwind.css"
+//   ],
+
+//   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+//   plugins: [
+//     { src: '~/plugins/vuex-persist', ssr: false },
+//     { src: '~plugins/nuxt-codemirror', ssr: false },
+//     { src: '~plugins/clickaway.js', ssr: false },
+//   ],
+
+//   // Auto import components: https://go.nuxtjs.dev/config-components
+//   components: true,
+
+//   // PWA module configuration: https://go.nuxtjs.dev/pwa
+//   pwa: {
+//     manifest: {
+//       lang: 'en'
+//     }
+//   },
+
+//   markdownit: {
+//     runtime: true,
+//     preset: 'default',
+//     html: true,
+//     linkify: true,
+//     xhtmlOut: true,
+//     breaks: true,
+//     typographer: true,
+//     use: [
+//       'markdown-it-div',
+//       'markdown-it-attrs',
+//       'markdown-it-emoji',
+//       'markdown-it-highlightjs'
+//     ]
+//   },
+// }
---
+++ store/index.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useMainStore = defineStore('main', {
+  state: () => ({
+    counter: 0,
+  }),
+  actions: {
+    increment() {
+      this.counter++
+    },
+  },
+})
