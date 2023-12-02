277cb4f2d51b087c03112b958f81e0558f974ded
ousszizou <odjaidri@gmail.com>
2023-12-02 16:11:38 +0100

    migrate to nuxt 3 - Meta Tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,14 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Arabic Markdown Editor",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   modules: [
-    '@pinia/nuxt',
+    "@pinia/nuxt",
     // '@nuxtjs/tailwindcss',
     // '@nuxtjs/pwa',
     // '@nuxtjs/markdownit'
@@ -12,19 +20,6 @@ export default defineNuxtConfig({
 
 //   // Target: https://go.nuxtjs.dev/config-target
 //   target: 'static',
-
-//   // Global page headers: https://go.nuxtjs.dev/config-head
-//   head: {
-//     title: 'markdown-editor',
-//     meta: [
-//       { charset: 'utf-8' },
-//       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-//       { hid: 'description', name: 'description', content: '' }
-//     ],
-//     link: [
-//       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-//     ]
-//   },
 
 //   // Global CSS: https://go.nuxtjs.dev/config-css
 //   css: [
