1f5b68c5837c69683d105f458b6ec52045e66a89
ousszizou <odjaidri@gmail.com>
2023-12-03 16:27:54 +0100

    rename previwContent file

---
+++ components/PreviewContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="markdown-content max-w-full"
+  ></div>
+</template>
+
+<script setup>
+// import { mapGetters } from "vuex";
+// export default {
+//   computed: {
+//     ...mapGetters("files", ["activeFile", "activeFileDetails"]),
+//   },
+// };
+</script>
