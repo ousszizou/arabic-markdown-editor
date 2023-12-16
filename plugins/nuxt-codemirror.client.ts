3be32f83b8b19c269f60cacbb7f2bd10f1c21bd8
ousszizou <odjaidri@gmail.com>
2023-12-16 05:02:45 +0100

    disable codemirror plugin temporary to make HMR works well

--- plugins/nuxt-codemirror.client.ts
+++ plugins/nuxt-codemirror.client.ts
@@ -1,13 +1,15 @@
-import { InstallCodemirro } from "codemirror-editor-vue3";
+// import { InstallCodemirro } from "codemirror-editor-vue3";
 
 // language
-import "codemirror/mode/javascript/javascript.js";
+// import "codemirror/mode/javascript/javascript.js";
 
 // codemirror active-line.js
-import "codemirror/addon/selection/active-line.js";
-import "codemirror/addon/scroll/simplescrollbars.js";
-import "codemirror/addon/edit/continuelist.js";
+// import "codemirror/addon/selection/active-line.js";
+// import "codemirror/addon/scroll/simplescrollbars.js";
+// import "codemirror/addon/edit/continuelist.js";
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(InstallCodemirro);
-});
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(InstallCodemirro);
+// });
+
+export { }
