14372283b07edf42a6a51c4b6f6985d1551683f9
ousszizou <odjaidri@gmail.com>
2024-01-04 07:21:33 +0100

    feat: add shadcn-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,11 +21,23 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "@nuxtjs/tailwindcss",
     "@nuxtjs/color-mode",
+    "shadcn-nuxt",
     // '@nuxtjs/pwa',
     // '@nuxtjs/markdownit'
   ],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
   },
 });
 // export default {
